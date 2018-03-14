9ce71f2bab8881a0f804fee9e90c4e89d935d4ef
Davidson <davilopescastro@gmail.com>
2018-03-14 14:11:28 -0300

    before eject

--- react-estudos/react-lab1/src/App.js
+++ react-estudos/react-lab1/src/App.js
@@ -1,49 +1,109 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person'
+import Person from './Person/Person';
+
 
 class App extends Component {
-  
   state = {
     persons: [
-      {name:'Max',age:28 },
-      {name:'Manu',age:29},
-      {name:'Davi',age:28}
-    ]
+      {id:1 ,name: 'Max', age: 28 },
+      {id:2 ,name: 'Manu', age: 29 },
+      {id:3 ,name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value',
+    showPersons: false,
   }
 
-  switchNameHandler = (newName) => {
-    this.setState({persons:[
-      {name:'Max',age:28 },
-      {name:newName,age:29},
-      {name:'Davi',age:47}
-    ] })
+ deletePersonHandler = (personIndex) => {
+   //slice copia o array
+   //pratica ruim tentar mudar o array sem copiar
+   //porque sao referencias entao esta mudando o state sem o setState
+  //const persons = this.state.persons.slice(); es5
+  const persons =[...this.state.persons];
+  persons.splice(personIndex,1);
+  this.setState({persons:persons})
+ }
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p =>{
+      return p.id===id;
+    });
+
+    const person ={
+      ...this.state.persons[personIndex]
+    };
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons:persons})
+
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({persons:[
-      {name:event.target.value,age:28 },
-      {name:'Manu',age:29},
-      {name:'Davi',age:47}
-    ] })
+  togglePersonHandler = () => {
+    const doesShow  = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
   }
 
-// Bonito mas perfomance ruim <button onClick={()=>this.switchNameHandler('testeArrow')} >Switch name</button>
-//Feio mas performance melhor <Person onClick={this.switchNameHandler.bind(this,'teste')}  name={this.state.persons[0].name} age={this.state.persons[0].age} />
+  render () {
+    const style = {
+      backgroundColor: 'green',
+      font: 'white',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
 
-  render() {
+    let persons = null;
+
+   
+    if(this.state.showPersons){
+      persons = (
+        <div>
+            {this.state.persons.map((person,index) => {
+              return <Person 
+              key={person.id}
+              click={this.deletePersonHandler.bind(this,index)} 
+              name={person.name} 
+              age={person.age}
+              changed={(event) => this.nameChangedHandler(event,person.id)} />
+              //changed={(event) => this.nameChangedHandler(event,person.id)}
+            })}
+         </div>
+      );
+
+      style.backgroundColor = 'red';
+    }
+
+
+    const classes = [];
+    if(this.state.persons.length<= 2){
+      classes.push('red');
+    }
+
+    if(this.state.persons.length <= 1){
+      classes.push('bold');
+    }
+    
+
     return (
+    
       <div className="App">
-        <h1>Hi i'm react app</h1>
-        
-        <button onClick={()=>this.switchNameHandler('testeArrow')} >Switch name</button>
-        <Person onClick={this.switchNameHandler.bind(this,'teste')}  name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} changed={this.nameChangedHandler.bind(this)} />
-        <Person click={this.switchNameHandler} name={this.state.persons[2].name} age={this.state.persons[2].age} />
-
+        <h1>Hi, I'm a React App</h1>
+        <p className={classes.join(' ')}>This is really working!</p>
+        <button 
+          style={style}
+          onClick={this.togglePersonHandler}>Toggle Persons</button>
+        {persons}
       </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
 export default App;
+
+// Bonito mas perfomance ruim <button onClick={()=>this.switchNameHandler('testeArrow')} >Switch name</button>
+//Feio mas performance melhor <Person onClick={this.switchNameHandler.bind(this,'teste')}  name={this.state.persons[0].name} age={this.state.persons[0].age} />
+
--- react-estudos/react-lab1/src/Person/Person.js
+++ react-estudos/react-lab1/src/Person/Person.js
@@ -1,13 +1,17 @@
 import React from 'react';
-const person = (props) => {
 
-return (
-    <div>
-        <p onClick={props.click}>I'm a Person {props.name} ! {props.age} year old! </p>
-        <p>{props.children}</p>
-        <input type="text" onChange={props.changed} value={props.name} />
-    </div>
-);
-}
+import './Person.css';
+
+const person = ( props ) => {
+  
+    
+    return (
+        <div className="Person" >
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
+        </div>
+    )
+};
 
 export default person;
